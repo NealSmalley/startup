6fa0b7451d43ef4dcfdf5f1521cb3309254b1354
NealSmalley <neal.e.smalley@gmail.com>
2024-04-12 23:28:34 -0600

    changes to other files

--- service/index.js
+++ service/index.js
@@ -49,9 +49,9 @@ apiRouter.post('/score', (req, res) => {
 });
 
 // Return the application's default page if the path is unknown
-app.use((_req, res) => {
-  res.sendFile('index.html', { root: 'public' });
-});
+//app.use((_req, res) => {
+//  res.sendFile('index.html', { root: 'public' });
+//});
 
 server = app.listen(port, () => {
   console.log(`Listening on port ${port}`);
