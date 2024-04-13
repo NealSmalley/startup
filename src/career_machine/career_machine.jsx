e49a558a03f37467b64a3eee8c78a9ed786c22be
NealSmalley <neal.e.smalley@gmail.com>
2024-04-12 23:27:46 -0600

    changes to career_machine and results.jsx

--- src/career_machine/career_machine.jsx
+++ src/career_machine/career_machine.jsx
@@ -1,19 +1,68 @@
 import React from 'react';
+import { useNavigate } from "react-router-dom";
+import { useEffect } from 'react';
+import '../chatClient.js';
 
 import './career_machine.css';
 
 export function Career_machine() {
+    const navigate = useNavigate();
+    const [career_machine_question, set_career_machine_question] = React.useState("Where would you like to work?")
+
+    function user_info() {
+    useEffect(() => {
+        const employmentEl = document.getElementById("employment").addEventListener("click", function(event){
+        const employmentElValue = document.getElementById("question").value
+      
+        const storedUserInfoString = localStorage.getItem("user_info");
+        const user_info = storedUserInfoString ? JSON.parse(storedUserInfoString) : {};
+        user_info.employer = employmentElValue; 
+        const updatedUserInfoString = JSON.stringify(user_info);
+        localStorage.setItem("user_info", updatedUserInfoString);
+        gotoLogin();
+        //window.location.href = "results"; 
+        });
+      attempt();
+      login();
+    });
+    }
+    function gotoLogin(){
+        navigate('/results')
+      }
+      
+      function login() {
+          var x = localStorage.getItem("userName");
+          //document.getElementById("career_machine_question").innerHTML = "Where would you like to work " + x +"?";
+          set_career_machine_question("Where would you like to work " + x +"?");
+          console.log(x);
+          const nameEl = document.querySelector("#question").value;
+          console.log(nameEl);
+          //gotoLogin();
+        }
+      function attempt() {
+        document.getElementById('employment').addEventListener('click', () => {
+          fetch('/api/attempts', { method: 'POST' })
+            .then(response => response.json())
+            .then(data => console.log('Attempt submitted:', data))
+            .catch(error => console.error('Error:', error));
+        });
+      }
+    
+      
+      
+      user_info()
+      
   return (
     <main>
     <hr></hr>
       <main>
-        <div class = "center">
-          <h5 id = "career_machine_question">Where would you like to work?</h5>
+        <div className = "center">
+          <h5 id = "career_machine_question">{career_machine_question}</h5>
             <input type="text" id="question" placeholder="Company here" />
             <button type="submit" id="employment">Enter</button>
         </div>
-      <div class="avatar">
-        <embed src="girl4.svg" width="300" height="380" embedborder="0" typle="img/svg+xml"/>
+      <div className="avatar">
+        {/*<embed src="girl4.svg" width="300" height="380" embedborder="0" typle="img/svg+xml"/>*/}
       </div>
       </main>
     </main>
--- src/results/results.jsx
+++ src/results/results.jsx
@@ -1,14 +1,73 @@
 import React from 'react';
 
 import './results.css';
+import '../chatClient';
 
 export function Results() {
+  const [messages, setMessages] = React.useState([{msg: "", from: "", cls: ""}]);
+  const [flexibility, setFlexibility] = React.useState("50");
+  const [pay, setPay] = React.useState("50");
+  const [transferability, setTransferability] = React.useState("50");
+  const [myName, setMyName] = React.useState("");
+  const isDisabled = myName === '';
+  
+        
+  const socket = React.useRef(null);
+
+  React.useEffect(() => {
+    // Adjust the webSocket protocol to what is being used for HTTP
+      const protocol = window.location.protocol === 'http:' ? 'ws' : 'wss';
+      socket.current = new WebSocket(`${protocol}://${window.location.host}/ws`);
+        // Display that we have opened the webSocket
+        socket.current.onopen = () => {
+          setMessages(prevMessages => [...prevMessages,{msg: "", from: "", cls: "connected"}]);
+          //setMessages('system', 'websocket', 'connected');
+        };
+    
+        // Display messages we receive from our friends
+        socket.current.onmessage = async (event) => {
+          const text = await event.data.text();
+          const chat = JSON.parse(text);
+          setMessages(prevMessages => [...prevMessages, {msg: chat.msg, from: chat.name, cls: 'friend'}]);
+        };
+    
+        // If the webSocket is closed then disable the interface
+        socket.current.onclose = () => {
+          setMessages(prevMessages => [...prevMessages, {msg:'disconnected', from: 'websocket', cls: 'disconnected'}]);
+          document.querySelector('#name-controls').disabled = true;
+          document.querySelector('#chat-controls').disabled = true;
+        };
+
+        return () => {
+          socket.current.close();
+        };
+      }, []);
+
+      function sendMessage() {
+        const msgEl = document.querySelector('#new-msg');
+        const msg = msgEl.value;
+        if (msg) {
+            setMessages(prevMessages => [...prevMessages, {msg, from: name, cls: 'me'}]);
+            const name = document.querySelector('#my-name').value;
+            console.log({name, msg})
+            socket.current.send(JSON.stringify({name, msg}));
+            msgEl.value = '';
+        }
+      }
+        // Send message on enter keystroke
+        //const input = document.querySelector('#new-msg');
+      function keydown(e) {
+          if (e.key === 'Enter') {
+              sendMessage();
+          }
+      };
+  
   return (
-    <main>
+    <main className="body">
           <hr></hr>
-    <div class="content">
-    <div class="first_third">
-      <h2 id = "employer">Employer:</h2>
+    <div className="content">
+    <div className="first_third">
+      <h2 id = "employer">Employer: {name}</h2>
       <p>Placeholder for Employer name(BYU)</p>
       <p> In demand skills for this Company:</p>
           <li>Coding</li>
@@ -22,33 +81,29 @@ export function Results() {
           <li>BYU Cyber Security Department</li>
               <li>$salary</li>
     </div>
-        <div class="avatar">
-          <embed src="girl4.svg" width="300" height="380" embedborder="0" typle="img/svg+xml"/>
+        <div className="avatar">
+          <embed src="../girl4.svg" width="300" height="380" embedborder="0" typle="img/svg+xml"/>
         </div>
-    <div class="study_plan">
-          <p>Study plan</p>
-          <li>Freshman</li>
-          <li>Sophomore</li>
-          <li>Junior</li>
-          <li>Senior</li>
-          <hr></hr>
-          <div class="name">
+    <div className="study_plan">
+          <div className="name">
             <fieldset id="name-controls">
               <legend>Your Name</legend>
-              <input id="my-name" type="text" />
+              <input id="my-name" value={myName} type="text" onChange={event => setMyName(event.target.value)}/>
             </fieldset>
           </div>
       
-          <fieldset id="chat-controls" disabled>
+          <fieldset id="chat-controls" disabled={isDisabled}>
             <legend>Chat about future careers with your friends!</legend>
-            <input id="new-msg" type="text" />
-            <button onclick="sendMessage()">Send</button>
+            <input id="new-msg" onKeyDown={keydown} type="text" />
+            <button onClick={sendMessage}>Send</button>
           </fieldset>
-          <div id="chat-text"></div>    
+          <div id="chat-text">
+            {messages.map((message, index) => (<span key={index} className={message.cls}>{message.from}, {message.msg}</span>))}
+          </div>    
     </div>
-      <div class="other_users">
+      <div className="other_users">
               <p>Friends Career results:</p>
-              <span class="place-holder-user">Unknown Friend</span>
+              <span className="place-holder-user">Unknown Friend</span>
             <table>
               <thead>
                   <tr>
@@ -81,39 +136,62 @@ export function Results() {
           </table>
             <hr></hr>
       </div>
-      <div class="extra_controls">
+      <div className="extra_controls">
         <p>Flexibility</p>
-        <div class="slidecontainer">
-          <div class="slider_border">
-            <div class="slider_background">
-            <input type="range" min="1" max="100" value="50" class="slider" id="myRange"></input>
+        <div className="slidecontainer">
+          <div className="slider_border">
+            <div className="slider_background">
+            <input type="range" min="1" max="100" value={flexibility} className="slider" id="myRange" onChange={event => setFlexibility(event.target.value)}></input>
             </div>
           </div>
         </div>
         <p>Pay/Hour</p>
-        <div class="slidecontainer">
-          <div class="slider_border">
-            <div class="slider_background">
-            <input type="range" min="1" max="100" value="50" class="slider" id="myRange"></input>
+        <div className="slidecontainer">
+          <div className="slider_border">
+            <div className="slider_background">
+            <input type="range" min="1" max="100" value={pay} className="slider" id="myRange" onChange={event => setPay(event.target.value)}></input>
             </div>
           </div>
         </div>
         <p>Transferability</p>
-        <div class="slidecontainer">
-          <div class="slider_border">
-            <div class="slider_background">
-            <input type="range" min="1" max="100" value="50" class="slider" id="myRange"></input>
+        <div className="slidecontainer">
+          <div className="slider_border">
+            <div className="slider_background">
+            <input type="range" min="1" max="100" value={transferability} className="slider" id="myRange" onChange={event => setTransferability(event.target.value)}></input>
             </div>
           </div>
         </div>
         <div>
       </div>
-        <form form method="get" action="career_machine.html">
-          <div class="restart_button"></div>
+        <form method="get" action="career_machine.html">
+          <div className="restart_button"></div>
             <button>Restart</button>
         </form>
       </div>
       </div>
       </main>
+
+      
     );
-}
+
+
+
+    // Send a message over the webSocket
+
+    // Create one long list of messages
+    //function appendMsg(cls, from, msg) {
+    //const chatText = document.querySelector('#chat-text');
+    //const chatEl = document.createElement('div');
+    //chatEl.innerHTML = `<span class="${cls}">${from}</span>: ${msg}</div>`;
+    //chatText.prepend(chatEl);
+    //}
+
+
+
+    // Disable chat if no name provided
+    //const chatControls = document.querySelector('#chat-controls');
+    //const myName = document.querySelector('#my-name');
+    //function keyup() {
+    //chatControls.disabled = myName.value === '';
+    //};
+  }
